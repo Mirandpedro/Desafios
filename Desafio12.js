c9dc4680b1903181e5ab6acd691cabbeb3a55988
Mirandpedro <ph12345p12345p@gmail.com>
2024-10-28 08:22:49 -0300

    Add files via upload

---
+++ Desafio12.js
@@ -0,0 +1,9 @@
+var jogahtml = window.document.getElementById('saida')
+
+function clique1() {
+    jogahtml.innerHTML += '<p> Você clicou escolheu a primeira opção = botão 1 </p>'
+       }
+
+       fuction cliquei(){
+        jogohml.innerHTML+= '<p> Você clicou escolheu a primeira opção = botão 2 </p>'
+       }
