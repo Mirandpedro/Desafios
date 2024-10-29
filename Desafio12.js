9f671d1137690d07bab2492ae9abfee7e2613117
Mirandpedro <ph12345p12345p@gmail.com>
2024-10-29 08:06:10 -0300

    Update Desafio12.js

--- Desafio12.js
+++ Desafio12.js
@@ -4,6 +4,22 @@ function clique1() {
     jogahtml.innerHTML += '<p> Você clicou escolheu a primeira opção = botão 1 </p>'
        }
 
-       fuction cliquei(){
+       fuction clique2(){
         jogohml.innerHTML+= '<p> Você clicou escolheu a primeira opção = botão 2 </p>'
        }
+
+function clique3 () {
+    jogohtml.innerhtml += '<p> você clicou escolheu a primeira opção = botão 3</p>'
+}
+
+function parmpar () {
+    let num = Number(window.prompt('Digite um número: '))
+    let tipo
+    if (num % 2 == 0) { 
+     tipo = '<strpng>PAR,/strong>'
+    } else { 
+       tipo = '<strong>ÍMPAR</strong>'
+    }
+
+let res = document.querySelector('section#result')
+    res.innerHTML = ' <p>0 número digitado foi ${num} e ele é: ${tipo}!</p>'
